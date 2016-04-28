91cd583b9beb6b5349c82dfc18c597bf4ac13e96
Spada Kao <jennykao123@gmail.com>
2016-04-28 15:20:45 +0800

    Initial Commit

---
+++ hw2Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw2Test
+//
+//  Created by Spada Kao on 2016/4/28.
+//  Copyright © 2016年 Spada Kao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
